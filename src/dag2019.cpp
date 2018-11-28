67746f7258f76b781fe55eb817b0f2ad1c6208a6
Carl Pearson <carl.w.pearson@gmail.com>
2018-11-27 21:23:46 -0600

    simplify dag2019 construction a bit

--- src/dag2019.cpp
+++ src/dag2019.cpp
@@ -27,13 +27,16 @@ DAG2019 DAG2019::from_edgelist(EdgeList &l)
         e.dst_ -= smallest;
     }
 
-    Int currentSrc = l.begin()->src_;
-
     for (const auto edge : l)
     {
 
-        if (dag.nodes_.empty() || (currentSrc != edge.src_))
+        assert(edge.src_ > 0);
+        assert(edge.dst_ > 0);
+
+        if (dag.nodes_.size() != size_t(edge.src_ + 1))
         {
+            assert(edge.src_ > dag.nodes_.back());
+            assert(edge.src_ == dag.sourceOffsets_.size());
             // LOG(trace, "node {} edges start at {}", edge.src_, dag.edgeSrc_.size());
             dag.nodes_.push_back(dag.edgeSrc_.size());
         }
@@ -45,34 +48,32 @@ DAG2019 DAG2019::from_edgelist(EdgeList &l)
             dag.edgeDst_.push_back(edge.dst_);
             // LOG(trace, "added edge {} ({} -> {})", dag.num_edges() - 1, edge.src_, edge.dst_);
         }
-
-        currentSrc = edge.src_;
     }
     dag.nodes_.push_back(dag.edgeSrc_.size());
     // LOG(trace, "final node idx {} points to {} ", dag.nodes_.size() - 1, dag.edgeSrc_.size());
 
-    // check that all nodes point to an edge or one past the end of the edge arrays
-    for (const auto n : dag.nodes_)
-    {
-        assert(n <= dag.edgeSrc_.size());
-        assert(n >= 0);
-    }
+    // // check that all nodes point to an edge or one past the end of the edge arrays
+    // for (const auto n : dag.nodes_)
+    // {
+    //     assert(n <= dag.edgeSrc_.size());
+    //     assert(n >= 0);
+    // }
 
-    //check that all edges have a src and dst which is a valid node
-    for (const auto s : dag.edgeSrc_)
-    {
-        assert(s < dag.nodes_.size());
-        assert(s >= 0);
-    }
-    for (const auto d : dag.edgeDst_)
-    {
-        if (d >= dag.nodes_.size())
-        {
-            LOG(critical, "edge dst {} is larger than the largest node {}", d, dag.nodes_.size());
-        }
-        assert(d < dag.nodes_.size());
-        assert(d >= 0);
-    }
+    // //check that all edges have a src and dst which is a valid node
+    // for (const auto s : dag.edgeSrc_)
+    // {
+    //     assert(s < dag.nodes_.size());
+    //     assert(s >= 0);
+    // }
+    // for (const auto d : dag.edgeDst_)
+    // {
+    //     if (d >= dag.nodes_.size())
+    //     {
+    //         LOG(critical, "edge dst {} is larger than the largest node {}", d, dag.nodes_.size());
+    //     }
+    //     assert(d < dag.nodes_.size());
+    //     assert(d >= 0);
+    // }
 
     return dag;
 }
