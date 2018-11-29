3ad9458fa93e3eda0cd0a8be6feea8b6a3095ce3
Carl Pearson <pearson@illinois.edu>
2018-11-29 15:52:33 -0600

    work on partition

--- src/dag_lowertriangular_csr.cpp
+++ src/dag_lowertriangular_csr.cpp
@@ -1,6 +1,7 @@
 #include "graph/dag_lowertriangular_csr.hpp"
 
 #include <map>
+#include <set>
 
 DAGLowerTriangularCSR DAGLowerTriangularCSR::from_edgelist(EdgeList &l)
 {
@@ -59,80 +60,66 @@ DAGLowerTriangularCSR DAGLowerTriangularCSR::from_edgelist(EdgeList &l)
 EdgeList DAGLowerTriangularCSR::get_node_edges(const VertexGroup &srcGroup, const VertexGroup &dstGroup) const
 {
 
-    // for (Int uIdx = 0; uIdx < num_nodes(); ++uIdx)
-    // {
-    //     const Int u = uIdx;
-    //     const Int vStart = sourceOffsets_[u];
-    //     const Int vEnd = sourceOffsets_[u + 1];
-    //     for (Int vOff = vStart; vOff < vEnd; ++vOff)
-    //     {
-    //         const Int v = destinationIndices_[vOff];
-    //         LOG(trace, "edge {} {}", u, v);
-    //     }
-    // }
-
-    EdgeList l;
-
-    // loop over u -> v edges
-    for (Int uIdx = 0; uIdx < num_nodes(); ++uIdx)
+    // collect all nodes in edges between nodes in srcGroup and nodes in dstGroup
+    std::set<Int> nodes;
+    for (Int u = 0; u < num_nodes(); ++u)
     {
-        const Int u = uIdx;
-        if (srcGroup.count(u))
+
+        const Int vStart = sourceOffsets_[u];
+        const Int vEnd = sourceOffsets_[u + 1];
+        for (Int vOff = vStart; vOff < vEnd; ++vOff)
+        {
+            const Int v = destinationIndices_[vOff];
+            if (srcGroup.count(u) && dstGroup.count(v))
+            {
+                nodes.insert(u);
+                nodes.insert(v);
+            }
+        }
+    }
+
+    // collect all edges that are between those nodes
+    EdgeList edges;
+    for (Int u = 0; u < num_nodes(); ++u)
+    {
+        if (nodes.count(u))
         {
             const Int vStart = sourceOffsets_[u];
             const Int vEnd = sourceOffsets_[u + 1];
             for (Int vOff = vStart; vOff < vEnd; ++vOff)
             {
                 const Int v = destinationIndices_[vOff];
-
-                // u -> v | u in src && v in dst
-                // v is in dst
-                if (dstGroup.count(v))
+                if (nodes.count(v))
                 {
-                    LOG(trace, "adding {0} -> {1} because {0} in src -> {1} in dst", u, v);
-                    l.push_back(Edge(u, v));
+                    edges.push_back(Edge(u, v));
                 }
-                else if (srcGroup.count(v))
-                {
+            }
+        }
+    }
 
-                    // u-> v | u in src v has a tail in dst
-                    const int dstStart = sourceOffsets_[v];
-                    const int dstEnd = sourceOffsets_[v + 1];
-                    for (Int dstOff = dstStart; dstOff != dstEnd; ++dstOff)
-                    {
-                        const Int dst = destinationIndices_[dstOff];
-                        if (dstGroup.count(dst))
-                        {
-                            LOG(trace, "adding {0} -> {1} because both in src and {1} has a tail in dst", u, v);
-                            l.push_back(Edge(u, v));
-                            break;
-                        }
-                    }
-                }
+    // also add edges vSrc -> vn ->vDst for vSrc != vDst
+    if (&srcGroup != &dstGroup)
+    { // FIXME, actually compare probably
 
-                // want all edges v -> w where w is a tail from src
-                const int wStart = sourceOffsets_[v];
-                const int wEnd = sourceOffsets_[v + 1];
-                for (Int wOff = wStart; wOff != wEnd; ++wOff)
+        for (Int u = 0; u < num_nodes(); ++u)
+        {
+            if (srcGroup.count(u)) // vSrc -> ...
+            {
+                const Int nStart = sourceOffsets_[u];
+                const Int nEnd = sourceOffsets_[u + 1];
+                for (Int nOff = nStart; nOff < nEnd; ++nOff)
                 {
-                    const Int w = destinationIndices_[wOff];
-                    bool found = false;
-                    for (auto head = srcGroup.begin(); head != srcGroup.end(); ++head)
+                    const Int n = destinationIndices_[nOff]; // vSrc -> vn
+
+                    const Int vStart = sourceOffsets_[n];
+                    const Int vEnd = sourceOffsets_[n + 1];
+                    for (Int vOff = vStart; vOff < vEnd; ++vOff)
                     {
-                        if (found)
-                            break;
-                        const int dstStart = sourceOffsets_[head];
-                        const int dstEnd = sourceOffsets_[head + 1];
-                        for (Int dstOff = dstStart; dstOff != dstEnd; ++dstOff)
+                        const Int v = destinationIndices_[vOff]; // vSrc -> vn -> vDst
+                        if (srcGroup.count(u) && dstGroup.count(v))
                         {
-                            const Int dst = destinationIndices_[dstOff];
-                            if (dst == w)
-                            {
-                                found = true;
-                                LOG(trace, "adding {0} -> {1} because {0} is a tail from src and {1} is a tail from src", v, w);
-                                l.push_back(Edge(v, w));
-                                break;
-                            }
+                            edges.push_back(Edge(u, n));
+                            edges.push_back(Edge(n, v));
                         }
                     }
                 }
@@ -140,7 +127,7 @@ EdgeList DAGLowerTriangularCSR::get_node_edges(const VertexGroup &srcGroup, cons
         }
     }
 
-    return l;
+    return edges;
 }
 
 std::vector<DAGLowerTriangularCSR> DAGLowerTriangularCSR::partition(const std::vector<VertexGroup *> &vertexGroups)
@@ -158,9 +145,11 @@ std::vector<DAGLowerTriangularCSR> DAGLowerTriangularCSR::partition(const std::v
 
             // these edges are only the edges src -> dst where src > dst
             // we need to add back in the dst -> src edges since EdgeList is assumed to have both
+            LOG(debug, "adding flipped edges");
             EdgeList flipped;
             for (const auto e : origEdges)
             {
+                assert(e.src_ > e.dst_);
                 flipped.push_back(Edge(e.dst_, e.src_));
             }
             for (const auto e : flipped)
@@ -169,29 +158,25 @@ std::vector<DAGLowerTriangularCSR> DAGLowerTriangularCSR::partition(const std::v
             }
 
             // sort by dst, then src to ensure that the new list is in increasing order of src
-            std::sort(origEdges.begin(), origEdges.end(),
-                      [](const Edge &a, const Edge &b) {
-                          return a.dst_ < b.dst_;
-                      });
-            std::stable_sort(origEdges.begin(), origEdges.end(),
-                             [](const Edge &a, const Edge &b) {
-                                 return a.src_ < b.src_;
-                             });
+            // LOG(debug, "sorting");
+            // std::sort(origEdges.begin(), origEdges.end(),
+            //           [](const Edge &a, const Edge &b) {
+            //               return a.dst_ < b.dst_;
+            //           });
+            // std::stable_sort(origEdges.begin(), origEdges.end(),
+            //                  [](const Edge &a, const Edge &b) {
+            //                      return a.src_ < b.src_;
+            //                  });
 
             // these edges do not necessarily have nodes with contiguous ids of 0 to whatever
             // rename the vertex ids.
             // the edges will already have src > dst
             // we need to preserve that during the renaming or we'll drop edges
             // generate rename by going through dsts first to ensure they will be less than the srcs
+
+            LOG(debug, "building rename map");
             std::map<Int, Int> rename;
             Int nextName = 0;
-            for (const auto &e : origEdges)
-            {
-                if (rename.count(e.dst_) == 0)
-                {
-                    rename[e.dst_] = nextName++;
-                }
-            }
             for (const auto &e : origEdges)
             {
                 if (rename.count(e.src_) == 0)
@@ -199,8 +184,16 @@ std::vector<DAGLowerTriangularCSR> DAGLowerTriangularCSR::partition(const std::v
                     rename[e.src_] = nextName++;
                 }
             }
+            for (const auto &e : origEdges)
+            {
+                if (rename.count(e.dst_) == 0)
+                {
+                    rename[e.dst_] = nextName++;
+                }
+            }
 
             // apply the rename to the list
+            LOG(debug, "renaming");
             for (auto &e : origEdges)
             {
                 e.src_ = rename[e.src_];
@@ -208,6 +201,7 @@ std::vector<DAGLowerTriangularCSR> DAGLowerTriangularCSR::partition(const std::v
             }
 
             // sort by dst, then src to ensure that the new list is in increasing order of src
+            LOG(debug, "sorting");
             std::sort(origEdges.begin(), origEdges.end(),
                       [](const Edge &a, const Edge &b) {
                           return a.dst_ < b.dst_;
