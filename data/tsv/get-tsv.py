28dae5ba3170c6e85bad8578148887753465e54e
Carl Pearson <pearson@illinois.edu>
2018-12-04 12:22:10 -0600

    don't extract files that are already extracted

--- data/tsv/get-tsv.py
+++ data/tsv/get-tsv.py
@@ -1,12 +1,13 @@
-#! /bin/env python
+#! /usr/bin/env python
 
 from __future__ import print_function
 import urllib, os
 import gzip
-from subprocess import call
+from subprocess import call, check_output
 
 URLS = [
 "https://graphchallenge.s3.amazonaws.com/synthetic/gc3/Theory-16-25-81-Bk.tsv",
+"https://graphchallenge.s3.amazonaws.com/synthetic/gc3/Theory-16-25-81-B1k.tsv",
 "https://graphchallenge.s3.amazonaws.com/synthetic/gc3/Theory-16-25-81-B2k.tsv",
 "https://graphchallenge.s3.amazonaws.com/snap/soc-Epinions1/soc-Epinions1_adj.tsv",
 "https://graphchallenge.s3.amazonaws.com/synthetic/graph500-scale22-ef16/graph500-scale22-ef16_adj.tsv.gz",
@@ -29,23 +30,36 @@ def download(url, dst):
   print(url, "->", dst)
   urllib.urlretrieve(url, dst)
 
+def get_extracted_size(path):
+  _, fileExtension = os.path.splitext(localFile)
+  if ".gz" == fileExtension:
+    cmd = ["gunzip", "-l", path]
+    output = check_output(cmd)
+    lines = output.splitlines()
+    sz = int(lines[1].split()[1])
+    return sz
+
 for url in URLS:
   remoteSize = get_remote_size(url)
   localFile = os.path.basename(url)
   localSize = get_local_size(localFile)
   if localSize != remoteSize:
-    print("getting", remoteSize/1024, "KB")
+    print(url, "->", localFile, "(", remoteSize/1024, "KB )")
     download(url, localFile)
   else:
-    print("already have", url)
+    print(localFile, "already exists and is the expected size (", localSize, ")")
 
   _, file_extension = os.path.splitext(localFile)
-  if file_extension == ".tar.gz":
-    call(['tar', '-xf', localFile])
-  elif file_extension == ".gz":
-    cmd = ['gunzip', "-k", "-f", localFile]
-    print("running", " ".join(cmd))
-    call(cmd)
+  if file_extension == ".gz":
+    extractedName = localFile[:-1 * len(file_extension)]
+    expectSz = get_extracted_size(localFile)
+    actualSz = get_local_size(extractedName)
+    if expectSz != actualSz:
+      cmd = ['gunzip', "-k", "-f", localFile]
+      print("running", " ".join(cmd))
+      call(cmd)
+    else:
+      print(extractedName, "already exists and is the expected size (", expectSz, ")")
 
 
 
