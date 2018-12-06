72615f932f5e993dddb537868406e309390b5f4d
Carl Pearson <carl.w.pearson@gmail.com>
2018-12-06 09:35:36 -0600

    add cpu info to run-all.sh

--- data/run-all.sh
+++ data/run-all.sh
@@ -3,6 +3,11 @@
 OUT=tri32_um_0.csv
 ERR=tri32_um_0.log
 
+nvcc --version | tee -a $ERR
+nvidia-smi | tee -a $ERR
+uname -a | tee -a $ERR
+lscpu | tee -a $ERR
+
 for tsv in tsv/*.tsv; do
   (../build/tri32 -m um -g 0 $tsv | tee -a $OUT) 2>&1 | tee -a $ERR;
 done
