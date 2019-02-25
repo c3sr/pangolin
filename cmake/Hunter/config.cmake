b1502186196efe54730fe1acfcbebb862d07c5ac
Carl Pearson <carl.w.pearson@gmail.com>
2019-02-25 08:20:38 -0600

    Catch2 v2.6.0

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -2,3 +2,4 @@
 # hunter-packages spdlog uses hunter to retrieve fmt
 hunter_config(spdlog VERSION 1.2.1-p0 CMAKE_ARGS CMAKE_POSITION_INDEPENDENT_CODE=ON)
 hunter_config(fmt VERSION 5.2.1 CMAKE_ARGS CMAKE_POSITION_INDEPENDENT_CODE=ON)
+hunter_config(catch2 VERSION 2.6.0 CMAKE_ARGS CMAKE_POSITION_INDEPENDENT_CODE=ON)
