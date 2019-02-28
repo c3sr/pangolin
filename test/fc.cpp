82bddb82d1d25f9bf3cef3994fd36fa0497d91ea
Carl Pearson <pearson@illinois.edu>
2019-02-28 14:23:15 -0600

    Complete graph generator

--- test/fc.cpp
+++ test/fc.cpp
@@ -5,14 +5,44 @@
 
 using namespace pangolin;
 
-TEST_CASE("Vector ctor(2)") {
+TEST_CASE("c2") {
 
   using NodeTy = int;
 
-  FullyConnected<NodeTy> fc(2);
-  auto i = fc.begin();
-  auto e = *i;
-  REQUIRE(e == EdgeTy<NodeTy>(0, 0));
-  i++;
-  REQUIRE(*i == EdgeTy<NodeTY>(0, 1));
+  generator::Complete<NodeTy> c2(2);
+
+  // all edges are within graph
+  for (auto e : c2) {
+    REQUIRE(e.first < 2);
+    REQUIRE(e.second < 2);
+  }
+
+  // expected number of edges
+  size_t count = 0;
+  for (auto _ : c2) {
+    ++count;
+  }
+  REQUIRE(count == 2);
+
+
+}
+
+TEST_CASE("c3") {
+
+  using NodeTy = int;
+
+  generator::Complete<NodeTy> c3(3);
+
+  // all edges are within graph
+  for (auto e : c3) {
+    REQUIRE(e.first < 3);
+    REQUIRE(e.second < 3);
+  }
+
+  // expected number of edges
+  size_t count = 0;
+  for (auto _ : c3) {
+    ++count;
+  }
+  REQUIRE(count == 6);
 }
