3dd8a2f0250e1ddafc10c477d27cad75d4370625
Carl Pearson <pearson@illinois.edu>
2019-02-28 16:59:30 -0600

    add uint64_t atomicAdd

---
+++ include/pangolin/atomic_add.cuh
@@ -0,0 +1,8 @@
+#pragma once
+
+__device__ inline uint64_t atomicAdd(uint64_t *p, uint64_t val) {
+    static_assert(sizeof(uint64_t) == sizeof(unsigned long long));
+    return static_cast<unsigned long long>(atomicAdd(
+        reinterpret_cast<unsigned long long*>(p),
+        static_cast<unsigned long long>(val)));
+}
