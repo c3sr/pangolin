579106dc76cb45c8c6320ff7586d356541d4a26f
Carl Pearson <carl.w.pearson@gmail.com>
2019-02-28 22:45:37 -0600

    add string to static_assert

--- include/pangolin/atomic_add.cuh
+++ include/pangolin/atomic_add.cuh
@@ -1,8 +1,7 @@
 #pragma once
 
 __device__ inline uint64_t atomicAdd(uint64_t *p, uint64_t val) {
-    static_assert(sizeof(uint64_t) == sizeof(unsigned long long));
-    return static_cast<unsigned long long>(atomicAdd(
-        reinterpret_cast<unsigned long long*>(p),
-        static_cast<unsigned long long>(val)));
+  static_assert(sizeof(uint64_t) == sizeof(unsigned long long), "expected uint64_t to be unsigned long long");
+  return static_cast<unsigned long long>(
+      atomicAdd(reinterpret_cast<unsigned long long *>(p), static_cast<unsigned long long>(val)));
 }
