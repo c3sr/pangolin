69a7d655b0b8d27c82483f56b3a11874c8103dd9
Carl Pearson <pearson@illinois.edu>
2019-03-05 11:59:30 -0600

    add a lower/upper bounds binary search

--- include/pangolin/algorithm/search.cuh
+++ include/pangolin/algorithm/search.cuh
@@ -25,4 +25,40 @@ __device__ static ulonglong2 serial_sorted_search_binary(const T *const array, s
   return make_ulonglong2(0, left);
 }
 
+enum class SearchBounds { UPPER, LOWER };
+
+/*! \brief return location of searchVal in array
+
+    \tparam bounds UPPER or LOWER bound of searchVal in array
+    \tparam It     random-access iterator into array
+    \tparam T      type of element to search for
+ */
+template <SearchBounds bounds, typename It, typename T>
+__device__ size_t binary_search(It array,           //!< [in] array to search
+                                const size_t count, //!< [in] size of array
+                                const T searchVal   //!< [in] value to search for
+) {
+  size_t left = 0;
+  size_t right = count;
+  while (left < right) {
+    const size_t mid = (left + right) / 2;
+    T val = array[mid];
+    bool pred;
+    switch (bounds) {
+    case SearchBounds::UPPER:
+      pred = val < searchVal;
+      break;
+    case SearchBounds::LOWER:
+      pred = !(searchVal < val);
+      break;
+    }
+    if (pred) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+  return left;
+}
+
 } // namespace pangolin
