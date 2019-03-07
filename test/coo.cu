eab7acd3599e883559e123e4cc421c63e654d5c3
Carl Pearson <carl.w.pearson@gmail.com>
2019-03-07 12:43:11 -0600

    more COO tests

--- test/coo.cu
+++ test/coo.cu
@@ -1,9 +1,11 @@
 #define CATCH_CONFIG_MAIN
 #include <catch2/catch.hpp>
 
-#include "pangolin/pangolin.hpp"
+#include "pangolin/sparse/coo.hpp"
 
 using namespace pangolin;
+
+TEST_CASE("ctor") { COO<int> coo; }
 
 TEST_CASE("COO<int>::from_edgelist") {
   INFO("ctor");
@@ -20,4 +22,22 @@ TEST_CASE("COO<int>::from_edgelist") {
   REQUIRE(coo.nnz() == 1);
   REQUIRE(coo.row_ind()[0] == 0);
   REQUIRE(coo.col_ind()[0] == 1);
+}
+
+TEST_CASE("COO<int>::from_edges upper triangular") {
+  std::vector<EdgeTy<uint64_t>> el = {{0, 1}, {0, 2}, {1, 0}, {1, 2}, {1, 3}, {1, 4}, {2, 0}, {2, 1},
+                                      {2, 3}, {2, 4}, {3, 1}, {3, 2}, {3, 4}, {4, 1}, {4, 2}, {4, 3}};
+
+  INFO("from_edgelist");
+  auto ut = [](EdgeTy<uint64_t> e) { return e.first < e.second; };
+  auto coo = COO<uint64_t>::from_edges(el.begin(), el.end(), ut);
+}
+
+TEST_CASE("COO<int>::from_edges lower triangular") {
+  std::vector<EdgeTy<uint64_t>> el = {{0, 1}, {0, 2}, {1, 0}, {1, 2}, {1, 3}, {1, 4}, {2, 0}, {2, 1},
+                                      {2, 3}, {2, 4}, {3, 1}, {3, 2}, {3, 4}, {4, 1}, {4, 2}, {4, 3}};
+
+  INFO("from_edgelist");
+  auto lt = [](EdgeTy<uint64_t> e) { return e.first > e.second; };
+  auto coo = COO<uint64_t>::from_edges(el.begin(), el.end(), lt);
 }
