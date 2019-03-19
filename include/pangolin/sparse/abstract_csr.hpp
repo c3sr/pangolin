0e502cb5aa42ff3cae5e2a4719f739019d506319
Carl Pearson <carl.w.pearson@gmail.com>
2019-03-19 10:22:39 -0500

    use abstract csr in unified-memory_csr

---
+++ include/pangolin/sparse/abstract_csr.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <cstdlib>
+#include <vector>
+
+#include "pangolin/types.hpp"
+
+template <typename INDEX> class CSR {
+public:
+  typedef INDEX index_type;
+  virtual const index_type *row_offsets() const = 0;
+  virtual const index_type *cols() const = 0;
+};
--- include/pangolin/sparse/unified_memory_csr.hpp
+++ include/pangolin/sparse/unified_memory_csr.hpp
@@ -4,29 +4,24 @@
 
 #include "pangolin/dense/cuda_managed_vector.hpp"
 #include "pangolin/edge_list.hpp"
-#include "pangolin/sparse/csr.hpp"
+#include "pangolin/sparse/abstract_csr.hpp"
 
 namespace pangolin {
 
 class UnifiedMemoryCSR : public CSR<Uint> {
 private:
-  CUDAManagedVector<index_type>
-      rowOffsets_; // offset that each row starts in data_ (num rows)
-  CUDAManagedVector<index_type> data_;  // the non-zero columns (nnz)
-  CUDAManagedVector<char> dataIsLocal_; // whether we own this column
+  CUDAManagedVector<index_type> rowOffsets_; // offset that each row starts in data_ (num rows)
+  CUDAManagedVector<index_type> data_;       // the non-zero columns (nnz)
+  CUDAManagedVector<char> dataIsLocal_;      // whether we own this column
 
 public:
-  virtual const index_type *row_offsets() const override {
-    return rowOffsets_.data();
-  }
+  virtual const index_type *row_offsets() const override { return rowOffsets_.data(); }
 
   virtual const index_type *cols() const override { return data_.data(); }
 
   virtual const char *is_local_cols() const { return dataIsLocal_.data(); }
 
-  virtual size_t num_rows() const {
-    return rowOffsets_.empty() ? 0 : rowOffsets_.size() - 1;
-  }
+  virtual size_t num_rows() const { return rowOffsets_.empty() ? 0 : rowOffsets_.size() - 1; }
 
   virtual size_t num_nonzero_rows() const {
     size_t count = 0;
@@ -51,11 +46,8 @@ public:
   }
 
   // static UnifiedMemoryCSR from_sorted_edgelist(const EdgeList &local);
-  static UnifiedMemoryCSR
-  from_sorted_edgelist(const EdgeList &local,
-                       const EdgeList &remote = EdgeList());
-  std::vector<UnifiedMemoryCSR>
-  partition_nonzeros(const size_t numPartitions) const;
+  static UnifiedMemoryCSR from_sorted_edgelist(const EdgeList &local, const EdgeList &remote = EdgeList());
+  std::vector<UnifiedMemoryCSR> partition_nonzeros(const size_t numPartitions) const;
 };
 
 } // namespace pangolin
