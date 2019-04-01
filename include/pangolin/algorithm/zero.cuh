08ce38bffa92962c6ffc034d5b4996c70275badc
Carl Pearson <pearson@illinois.edu>
2019-04-01 18:22:43 -0500

    more debug in zero

--- include/pangolin/algorithm/zero.cuh
+++ include/pangolin/algorithm/zero.cuh
@@ -65,7 +65,8 @@ template <size_t N, typename T> void zero_async(T *ptr, const int dev, cudaStrea
   CUDA_RUNTIME(cudaSetDevice(dev));
   constexpr size_t dimBlock = 512;
   constexpr size_t dimGrid = (dimBlock + N - 1) / dimBlock;
-  SPDLOG_DEBUG(logger::console, "launch zero: device = {}, blocks = {}, threads = {}", dev, dimGrid, dimBlock);
+  SPDLOG_DEBUG(logger::console, "launch zero: device = {}, blocks = {}, threads = {} stream = {}", dev, dimGrid,
+               dimBlock, uintptr_t(stream));
   zero<N, dimGrid, dimBlock><<<dimGrid, dimBlock, 0, stream>>>(ptr);
   CUDA_RUNTIME(cudaGetLastError());
 }
