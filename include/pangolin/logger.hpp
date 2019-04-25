43aebc7671f25bef3fbb3431618f2c0b3a53dbd3
Carl Pearson <pearson@illinois.edu>
2019-04-25 14:18:55 -0500

    don't build pangolin shared libraries

--- include/pangolin/logger.hpp
+++ include/pangolin/logger.hpp
@@ -1,13 +1,14 @@
 
 #pragma once
 
-#include "namespace.hpp"
 #include <spdlog/spdlog.h>
+#include <spdlog/sinks/stdout_color_sinks.h>
 
 namespace pangolin {
 
 namespace logger {
-extern std::shared_ptr<spdlog::logger> console;
+std::shared_ptr<spdlog::logger> console = spdlog::stderr_color_mt("pangolin");
+
 
 /*!
 Log levels used by pangolin's internal logger.
@@ -15,7 +16,39 @@ Log levels used by pangolin's internal logger.
 Trace-level messages are disabled during release builds.
 */
 enum Level { TRACE, DEBUG, INFO, WARN, ERR, CRITICAL };
-void set_level(const Level &level);
+
+
+/*! Set the log level of pangolin's internal logger
+ 
+*/
+inline void set_level(const Level &level) {
+  switch (level) {
+  case TRACE: {
+    console->set_level(spdlog::level::trace);
+    return;
+  }
+  case DEBUG: {
+    console->set_level(spdlog::level::debug);
+    return;
+  }
+  case INFO: {
+    console->set_level(spdlog::level::info);
+    return;
+  }
+  case WARN: {
+    console->set_level(spdlog::level::warn);
+    return;
+  }
+  case ERR: {
+    console->set_level(spdlog::level::err);
+    return;
+  }
+  case CRITICAL: {
+    console->set_level(spdlog::level::critical);
+    return;
+  }
+  }
+}
 
 } // namespace logger
 
