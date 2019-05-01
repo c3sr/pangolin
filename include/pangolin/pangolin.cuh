3e95bdf4e3a3620043feca2fa76041f1579f715b
Carl Pearson <pearson@illinois.edu>
2019-05-01 13:56:30 -0500

    remove algorithm/ktruss_binary.cuh from single header

--- include/pangolin/pangolin.cuh
+++ include/pangolin/pangolin.cuh
@@ -1,8 +1,9 @@
 #pragma once
 
+// #include "algorithm/ktruss_binary.cuh"
+#include "algorithm/ktruss_serial.cuh"
 #include "algorithm/tc_edge_binary.cuh"
 #include "algorithm/tc_edge_dynpar.cuh"
 #include "algorithm/tc_edge_linear.cuh"
+#include "algorithm/tc_vertex_block_binary.cuh"
 #include "dense/vector.hu"
-#include "algorithm/ktruss_serial.cuh"
-#include "algorithm/ktruss_binary.cuh"
