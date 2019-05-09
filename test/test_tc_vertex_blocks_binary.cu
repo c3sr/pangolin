2605a034239042e8aea789a6d6bd5d6e4f62fdba
Carl Pearson <carl.w.pearson@gmail.com>
2019-05-09 12:05:19 -0500

    add row-partitioned test

--- test/test_tc_vertex_blocks_binary.cu
+++ test/test_tc_vertex_blocks_binary.cu
@@ -12,12 +12,11 @@ using namespace pangolin;
 
 TEST_CASE("ctor", "[gpu]") {
   pangolin::init();
-  logger::set_level(logger::Level::INFO);
+  logger::set_level(logger::Level::DEBUG);
   VertexBlocksBinaryTC c;
   REQUIRE(c.count() == 0);
 
   SECTION("complete(3)", "[gpu]") {
-    logger::set_level(logger::Level::DEBUG);
     using NodeTy = int;
 
     // complete graph with 3 nodes
@@ -32,7 +31,6 @@ TEST_CASE("ctor", "[gpu]") {
   }
 
   SECTION("complete(4)", "[gpu]") {
-    logger::set_level(logger::Level::DEBUG);
     using NodeTy = int;
 
     // complete graph with 4 nodes
@@ -44,5 +42,24 @@ TEST_CASE("ctor", "[gpu]") {
     REQUIRE(csr.nnz() == 6);
     REQUIRE(c.count() == 0);
     REQUIRE(4 == c.count_sync(csr.view()));
+  }
+
+  SECTION("complete(4) row partition", "[gpu]") {
+    using NodeTy = int;
+
+    VertexBlocksBinaryTC cs[2];
+    REQUIRE(cs[0].count() == 0);
+    REQUIRE(cs[1].count() == 0);
+
+    // complete graph with 4 nodes
+    generator::Complete<NodeTy> g(4);
+
+    auto keep = [](EdgeTy<NodeTy> e) { return e.first < e.second; };
+    auto csr = CSR<NodeTy>::from_edges(g.begin(), g.end(), keep);
+    REQUIRE(csr.nnz() == 6);
+
+    uint64_t a = cs[0].count_sync(csr.view(), 0, 2); // first 2 rows
+    uint64_t b = cs[1].count_sync(csr.view(), 2, 2); // next 2 rows
+    REQUIRE(4 == a + b);
   }
 }
