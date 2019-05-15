d080b511e9b3756d4655d164361571782ba8623a
Dae Hee Kim <kim678@minsky2-1.csl.illinois.edu>
2019-05-15 16:25:53 -0500

    CPU-GPU version added, bug fixed for cudaDeviceSync

--- include/pangolin/algorithm/cross_decomp_gcpu.cuh
+++ include/pangolin/algorithm/cross_decomp_gcpu.cuh
@@ -9,13 +9,13 @@ __global__ void __launch_bounds__(1024,2) repartition_kernel(   uint8_t* orig_P,
                                                                 uint32_t* coo_col, uint32_t* coo_col_ptr,
                                                                 const uint64_t num_nodes, 
                                                                 const float h, uint64_t part_size,
-                                                                bool is_divisible, uint64_t cpu_por)
+                                                                bool is_divisible)
 {
     uint64_t idx = blockIdx.x*blockDim.x+threadIdx.x;
     
-	if(idx < num_nodes-cpu_por)
+	if(idx < num_nodes)
 	{		
-        uint64_t cur_node = idx+cpu_por;
+        uint64_t cur_node = idx;
         uint64_t connected_and_in_curpart[PARTITION] = {0};
         uint64_t degree = (coo_col_ptr[cur_node+1] - coo_col_ptr[cur_node]);
         uint32_t start_cur_col_ptr = coo_col_ptr[cur_node];
@@ -110,9 +110,8 @@ template<typename pangolinCOO, uint8_t PARTITION>
 class CrossDecomp{
     private:
         float h;
-        bool is_divisible = true, use_cpu=false;
-        uint32_t num_nodes, part_size, cpu_por;
-        uint64_t num_edges;
+        bool is_divisible = true;
+        uint32_t num_nodes, part_size;
         uint32_t fixed_array[PARTITION];//this array contains partition sizes to initialize cardi arrays
         Vector<uint8_t> row_P, col_P;
         Vector<uint32_t> row_cardi, row_new_cardi, col_cardi, col_new_cardi;
@@ -121,7 +120,7 @@ class CrossDecomp{
         
     public:
         CrossDecomp(){};
-        void CrossDecompInit(bool cpu, pangolinCOO COO);
+        void CrossDecompInit(pangolinCOO COO);
         void Host_repartition_async(bool one_iter, int num_iter, pangolinCOO COO);
         void Host_repartition_sync(bool one_iter, int num_iter, pangolinCOO COO);
         void initParts(uint8_t* P, uint32_t* cardi);
@@ -130,22 +129,17 @@ class CrossDecomp{
         void Host_evalEdges_sync(pangolinCOO COO);
         void printEdgesPerPar();
         void rename();
-        void cal_CPU_portion(pangolinCOO COO);
-        void cpu_repartition_kernel(uint8_t* orig_P, uint8_t* new_P, 
-                                    uint32_t* cardi, uint32_t* new_cardi,
-                                    uint32_t* coo_col, uint32_t* coo_col_ptr);
 };
 
 /* ! Initialization function for Cross-Decomposition
     This function must be called after instantiating CrossDecomp obj
 */
 template<typename pangolinCOO, uint8_t PARTITION>
-void CrossDecomp<pangolinCOO, PARTITION>::CrossDecompInit(bool cpu, pangolinCOO COO)
+void CrossDecomp<pangolinCOO, PARTITION>::CrossDecompInit(pangolinCOO COO)
 {
     using namespace std;
     h = 0.9;
     num_nodes = COO.num_nodes();
-    num_edges = COO.nnz();
 
     row_P.resize(num_nodes);
     col_P.resize(num_nodes);
@@ -171,8 +165,6 @@ void CrossDecomp<pangolinCOO, PARTITION>::CrossDecompInit(bool cpu, pangolinCOO 
         cout<<"N is divided perfectly"<<endl;
         cout<<"Size of Each Partition is: "<<part_size<<endl;
     }
-    use_cpu = cpu;
-    cal_CPU_portion(COO);
 }
 
 /* ! Initialize the partitions for each node in the graph 
@@ -200,7 +192,7 @@ void CrossDecomp<pangolinCOO, PARTITION>::initParts(uint8_t* P, uint32_t* cardi)
 template<typename pangolinCOO, uint8_t PARTITION>
 void CrossDecomp<pangolinCOO, PARTITION>::Host_repartition_async(bool one_iter, int num_iter, pangolinCOO COO)
 {
-    dim3 dimGrid(ceil(((float)(num_nodes-cpu_por))/1024),1,1);
+    dim3 dimGrid(ceil(((float)num_nodes)/1024),1,1);
     dim3 dimBlock(1024,1,1); //1024
 
     if(one_iter){
@@ -208,12 +200,8 @@ void CrossDecomp<pangolinCOO, PARTITION>::Host_repartition_async(bool one_iter, 
                                                                 col_cardi.data(), col_new_cardi.data(),
                                                                 COO.colInd_.data(), COO.rowPtr_.data(), 
                                                                 num_nodes, h, part_size,
-                                                                is_divisible, cpu_por);
+                                                                is_divisible);
         CUDA_RUNTIME(cudaGetLastError());   
-        if(use_cpu)
-            cpu_repartition_kernel( col_P.data(), row_P.data(), 
-                                    col_cardi.data(), col_new_cardi.data(),
-                                    COO.colInd_.data(), COO.rowPtr_.data());
     }
     else{
         for(int i=0; i<num_iter; i++){
@@ -221,18 +209,14 @@ void CrossDecomp<pangolinCOO, PARTITION>::Host_repartition_async(bool one_iter, 
                                                                     row_cardi.data(), row_new_cardi.data(),
                                                                     COO.colInd_.data(), COO.rowPtr_.data(), 
                                                                     num_nodes, h, part_size,
-                                                                    is_divisible, cpu_por);
+                                                                    is_divisible);
             CUDA_RUNTIME(cudaGetLastError());
             repartition_kernel<PARTITION><<<dimGrid, dimBlock>>>(   col_P.data(), row_P.data(), 
                                                                     col_cardi.data(), col_new_cardi.data(),
                                                                     COO.colInd_.data(), COO.rowPtr_.data(), 
                                                                     num_nodes, h, part_size,
-                                                                    is_divisible, cpu_por);
+                                                                    is_divisible);
             CUDA_RUNTIME(cudaGetLastError());
-            if(use_cpu)
-                cpu_repartition_kernel( col_P.data(), row_P.data(), 
-                                        col_cardi.data(), col_new_cardi.data(),
-                                        COO.colInd_.data(), COO.rowPtr_.data());
             CUDA_RUNTIME(cudaDeviceSynchronize());
             reset_cardi();
         }
@@ -297,110 +281,6 @@ void CrossDecomp<pangolinCOO, PARTITION>::Host_evalEdges_sync(pangolinCOO COO)
     return;
 }
 
-template<typename pangolinCOO, uint8_t PARTITION>
-void CrossDecomp<pangolinCOO, PARTITION>::cal_CPU_portion(pangolinCOO COO)
-{
-    // if(!use_cpu){
-    //     std::cout<<"Not using CPU!"<<cpu_por<<std::endl;
-    //     cpu_por = 0;
-    //     return;
-    // }
-    // uint64_t edgePortion = uint64_t(num_edges * 0.1);
-    // cpu_por = 0;
-    // while(COO.rowPtr_[cpu_por] < edgePortion){
-    //     cpu_por++;
-    // }
-    // std::cout<<"CPU Portion is: "<<cpu_por<<std::endl;
-    if(use_cpu)
-        cpu_por = 100;
-    else
-        cpu_por = 0;
-    return;
-}
-
-
-
-
-template<typename pangolinCOO, uint8_t PARTITION>
-void CrossDecomp<pangolinCOO, PARTITION>::cpu_repartition_kernel(   uint8_t* orig_P, uint8_t* new_P, 
-                                                                    uint32_t* cardi, uint32_t* new_cardi,
-                                                                    uint32_t* coo_col, uint32_t* coo_col_ptr
-                                                                )
-{
-    //uint64_t idx = blockIdx.x*blockDim.x+threadIdx.x;
-	for(uint64_t cur_node=0; cur_node<cpu_por; cur_node++)
-	{		
-        //uint64_t cur_node = idx;
-        uint64_t connected_and_in_curpart[PARTITION] = {0};
-        uint64_t degree = (coo_col_ptr[cur_node+1] - coo_col_ptr[cur_node]);
-        uint32_t start_cur_col_ptr = coo_col_ptr[cur_node];
-
-        //calculate the nodes that are connected to cur_node and in same current partition
-        for(uint64_t j=0; j<degree; j++){
-            uint32_t cur_col_ptr = start_cur_col_ptr + j;
-            uint32_t cur_col = coo_col[cur_col_ptr];
-            for (uint8_t cur_part = 0; cur_part < PARTITION; cur_part++) {
-               connected_and_in_curpart[cur_part] += (orig_P[cur_col] == cur_part);
-            }
-        }
-        
-        float cost[PARTITION] = {0};
-        for (uint8_t i = 0; i < PARTITION; i++){
-            cost[i] = h* connected_and_in_curpart[i] + 
-                        (1-h)*(num_nodes - (cardi[i] + degree - connected_and_in_curpart[i]));
-        } 
-
-        //initialize arg_sort array
-        uint8_t arg_sort[PARTITION];
-        for (uint8_t i = 0; i < PARTITION; i++)
-            arg_sort[i] = i;
-        
-        //perform arg_sort with cost
-        for (uint8_t i = 0; i < PARTITION-1; i++){       
-            for (uint8_t j = 0; j < PARTITION-i-1; j++){
-                if (cost[j] < cost[j+1]){
-                    float temp = cost[j];
-                    cost[j] = cost[j+1];
-                    cost[j+1] = temp;
-                    uint8_t temp2 = arg_sort[j];
-                    arg_sort[j] = arg_sort[j+1];
-                    arg_sort[j+1] = temp2;
-                }
-            }
-        }
-
-        unsigned int old_size;
-        for (uint8_t i = 0; i < PARTITION; i++){
-            if(!is_divisible){
-                if(arg_sort[i]==PARTITION-1){
-                    old_size = __sync_fetch_and_add((unsigned int*)&new_cardi[arg_sort[i]],(unsigned int) 1);
-                    if(old_size < part_size+(num_nodes%PARTITION)){
-                        new_P[cur_node] = arg_sort[i];
-                        break;
-                    }
-                }
-                else{
-                    old_size = __sync_fetch_and_add((unsigned int*)&new_cardi[arg_sort[i]],(unsigned int) 1);
-                    if(old_size < part_size){
-                        new_P[cur_node] = arg_sort[i];
-                        break;
-                    }
-                }
-            }
-            else{
-                old_size = __sync_fetch_and_add((unsigned int*)&new_cardi[arg_sort[i]],(unsigned int) 1);
-                if(old_size < part_size){
-                    new_P[cur_node] = arg_sort[i];
-                    break;
-                }
-            }
-        }
-    }
-}
-
-
-
-
 /* ! This function simply prints out the result of evalEdges to terminal
 */
 template<typename pangolinCOO, uint8_t PARTITION>
@@ -452,5 +332,4 @@ void CrossDecomp<pangolinCOO, PARTITION>::rename()
 
 
 }//namespace Pangolin
-
 
