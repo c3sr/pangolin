d1c66ae4dea70f4119df3d167fd27f07a1bb9d7e
Carl Pearson <pearson@illinois.edu>
2019-06-06 14:17:13 -0500

    add block size parameter

--- include/pangolin/algorithm/tc_edge_binary.cuh
+++ include/pangolin/algorithm/tc_edge_binary.cuh
@@ -8,10 +8,11 @@
 #include "search.cuh"
 
 template <size_t BLOCK_DIM_X, size_t C, typename CsrCooView>
-__global__ void kernel(uint64_t *count,                             //!< [inout] the count, caller should zero
-                       const CsrCooView mat, const size_t numEdges, //!< the number of edges this kernel will count
-                       const size_t edgeStart                       //!< the edge this kernel will start counting at
-) {
+__launch_bounds__(BLOCK_DIM_X) __global__ void __launch_bounds__(BLOCK_DIM_X)
+    kernel(uint64_t *__restrict__ count,                //!< [inout] the count, caller should zero
+           const CsrCooView mat, const size_t numEdges, //!< the number of edges this kernel will count
+           const size_t edgeStart                       //!< the edge this kernel will start counting at
+    ) {
 
   typedef typename CsrCooView::index_type Index;
 
@@ -71,36 +72,55 @@ public:
 
   BinaryTC() : BinaryTC(0) {}
 
+  /* Async count triangle on device. May return before count is complete.
+
+    Call sync() to block until count is complete.
+    Call count() to retrieve count
+
+  */
   template <typename CsrCoo>
-  void count_async(const CsrCoo &mat, const size_t numEdges, const size_t edgeOffset = 0, const size_t c = 1) {
+  void count_async(const CsrCoo &mat, const size_t numEdges, const size_t edgeOffset = 0, const size_t dimBlock = 256,
+                   const size_t c = 1) {
+
     zero_async<1>(count_, dev_, stream_); // zero on the device that will do the counting
+
     // create one warp per edge
-    constexpr int dimBlock = 256;
     const int dimGrid = (32 * numEdges + (dimBlock * c) - 1) / (dimBlock * c);
     assert(edgeOffset + numEdges <= mat.nnz());
     assert(count_);
     LOG(debug, "device = {}, blocks = {}, threads = {}", dev_, dimGrid, dimBlock);
     CUDA_RUNTIME(cudaSetDevice(dev_));
-    switch (c) {
-    case 1:
-      kernel<dimBlock, 1><<<dimGrid, dimBlock, 0, stream_>>>(count_, mat, numEdges, edgeOffset);
-      break;
-    case 2:
-      kernel<dimBlock, 2><<<dimGrid, dimBlock, 0, stream_>>>(count_, mat, numEdges, edgeOffset);
-      break;
-    case 4:
-      kernel<dimBlock, 4><<<dimGrid, dimBlock, 0, stream_>>>(count_, mat, numEdges, edgeOffset);
-      break;
-    case 8:
-      kernel<dimBlock, 8><<<dimGrid, dimBlock, 0, stream_>>>(count_, mat, numEdges, edgeOffset);
-      break;
-    case 16:
-      kernel<dimBlock, 16><<<dimGrid, dimBlock, 0, stream_>>>(count_, mat, numEdges, edgeOffset);
-      break;
-    default:
-      LOG(critical, "unsupported coarsening factor, try 1,2,4,8,16");
+
+#define IF_CASE(const_dimBlock, const_c)                                                                               \
+  if (dimBlock == const_dimBlock && c == const_c) {                                                                    \
+    kernel<const_dimBlock, const_c><<<dimGrid, const_dimBlock, 0, stream_>>>(count_, mat, numEdges, edgeOffset);       \
+  }
+
+#define ELSE_IF_CASE(const_dimBlock, const_c)                                                                          \
+  else if (dimBlock == const_dimBlock && c == const_c) {                                                               \
+    kernel<const_dimBlock, const_c><<<dimGrid, const_dimBlock, 0, stream_>>>(count_, mat, numEdges, edgeOffset);       \
+  }
+
+    IF_CASE(32, 1)
+    ELSE_IF_CASE(64, 1)
+    ELSE_IF_CASE(128, 1)
+    ELSE_IF_CASE(256, 1)
+    ELSE_IF_CASE(512, 1)
+    ELSE_IF_CASE(1024, 1)
+    ELSE_IF_CASE(32, 2)
+    ELSE_IF_CASE(64, 2)
+    ELSE_IF_CASE(128, 2)
+    ELSE_IF_CASE(256, 2)
+    ELSE_IF_CASE(512, 2)
+    ELSE_IF_CASE(1024, 2)
+    else {
+      LOG(critical, "unsupported coarsening factor {} or block dimension {}", c, dimBlock);
       exit(-1);
     }
+
+#undef IF_CASE
+#undef ELSE_IF_CASE
+
     CUDA_RUNTIME(cudaGetLastError());
   }
 
