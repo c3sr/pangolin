b8dd5b6419a22c3ccdfef8dacb3a1eff2aaa153f
Carl Pearson <pearson@illinois.edu>
2019-06-10 10:46:42 -0500

    performance tuning and bug fixing

--- include/pangolin/algorithm/tc_edge_dyn.cuh
+++ include/pangolin/algorithm/tc_edge_dyn.cuh
@@ -9,6 +9,7 @@
 #include "count.cuh"
 #include "pangolin/algorithm/broadcast.cuh"
 #include "pangolin/algorithm/fill.cuh"
+#include "pangolin/algorithm/reduction.cuh"
 #include "pangolin/algorithm/zero.cuh"
 #include "pangolin/dense/device_buffer.cuh"
 #include "pangolin/dense/vector.hu"
@@ -24,6 +25,10 @@ __global__ void __launch_bounds__(BLOCK_DIM_X)
     ) {
 
   typedef typename CsrCooView::index_type Index;
+
+  // increase the scale the computed binary cost by this amount when deciding which algorithm
+  // 0.125: ~binary on scale 23, ~linear on cit-Patents (RTX 6000)
+  constexpr float FAVOR_LINEAR = 0.125;
 
   static_assert(BLOCK_DIM_X % 32 == 0, "block size should be multiple of 32");
   constexpr size_t warpsPerBlock = BLOCK_DIM_X / 32;
@@ -85,6 +90,8 @@ __global__ void __launch_bounds__(BLOCK_DIM_X)
     const Index *dstEnd = nullptr;
     Index srcSz = 0;
     Index dstSz = 0;
+    size_t edgeLinearCost = 0;
+    size_t edgeBinaryCost = 0;
     if (i < edgeStart + numEdges) {
       const Index src = adj.rowInd_[i];
       const Index dst = adj.colInd_[i];
@@ -96,32 +103,45 @@ __global__ void __launch_bounds__(BLOCK_DIM_X)
       dstEnd = &adj.colInd_[adj.rowPtr_[dst + 1]];
       dstSz = dstEnd - dstBegin;
 
-      // FIXME: better warp reduction?
       // contribute the linear cost of my edge to the total
-      size_t edgeLinearCost = srcSz + dstSz;
-      atomicAdd(&linearCost[wx], edgeLinearCost);
+      if (srcSz > 0 && dstSz > 0) {
+      edgeLinearCost = srcSz + dstSz;
+      }
 
       // contribute the binary cost of my edge to the total
-      // FIXME: better warp reduction?
-      size_t edgeBinaryCost;
       if (srcSz > dstSz) {
         edgeBinaryCost = dstSz * (sizeof(srcSz) * CHAR_BIT - __clz(srcSz));
       } else {
         edgeBinaryCost = srcSz * (sizeof(dstSz) * CHAR_BIT - __clz(dstSz));
       }
-      atomicAdd(&binaryCost[wx], edgeBinaryCost);
     }
+    size_t warpLinearCost = pangolin::warp_sum(edgeLinearCost);
+    if (0 == lx) {
+      linearCost[wx] = warpLinearCost;
+    }
+    size_t warpBinaryCost = pangolin::warp_sum(edgeBinaryCost);
+    if (0 == lx) {
+      binaryCost[wx] = warpBinaryCost;
+    }
+    // atomicAdd(&linearCost[wx], edgeLinearCost);
+    // atomicAdd(&binaryCost[wx], edgeBinaryCost);
     __syncwarp(); // wait for all threads in the warp to have contributed to the cost
     // if (lx == 0) {
     //   printf("warp %lu @ edge %lu: linear %lu binary %lu\n", wx, warpEdgeIdx, linearCost[wx], binaryCost[wx]);
     // }
 
     // based on estimated costs, choose which approach all threads will take
-    if (linearCost < binaryCost) {
+    if (linearCost[wx] <= FAVOR_LINEAR * binaryCost[wx]) {
+      // if (lx == 0) {
+      //   printf("linear\n");
+      // }
       // use one thread per edge to do the linear search
       // lanes without an edge have nullptrs for begin and end, so they won't count;
       threadCount += pangolin::serial_sorted_count_linear(srcBegin, srcEnd, dstBegin, dstEnd);
     } else {
+      // if (lx == 0) {
+      //   printf("binary %lu %lu\n", linearCost[wx], binaryCost[wx]);
+      // }
       // use all threads in the warp to do a parallel binary search for each edge
       for (size_t j = warpEdgeIdx; j < warpEdgeIdx + 32 && j < edgeStart + numEdges; ++j) {
 
@@ -160,7 +180,7 @@ __global__ void __launch_bounds__(BLOCK_DIM_X)
   //   printf("warp %lu lane %lu tris total %lu \n", wx, lx, threadCount);
   // }
 
-  __syncthreads();
+  // __syncthreads();
   // Block-wide reduction of threadCount
   typedef cub::BlockReduce<uint64_t, BLOCK_DIM_X> BlockReduce;
   __shared__ typename BlockReduce::TempStorage tempStorage;
@@ -187,8 +207,7 @@ private:
   bool destroyStream_;
 
 public:
-  EdgeWarpDynTC(int dev)
-      : dev_(dev), stream_(nullptr), count_(nullptr), edgeIdx_(DeviceBuffer<size_t>(1, dev)), destroyStream_(true) {
+  EdgeWarpDynTC(int dev) : dev_(dev), stream_(nullptr), count_(nullptr), edgeIdx_(1, dev), destroyStream_(true) {
     SPDLOG_TRACE(logger::console(), "set dev {}", dev_);
     CUDA_RUNTIME(cudaSetDevice(dev_));
     SPDLOG_TRACE(logger::console(), "create stream");
@@ -202,7 +221,8 @@ public:
     // *count_ = 0;
   }
 
-  EdgeWarpDynTC(int dev, cudaStream_t stream) : dev_(dev), stream_(stream), count_(nullptr), destroyStream_(false) {
+  EdgeWarpDynTC(int dev, cudaStream_t stream)
+      : dev_(dev), stream_(stream), count_(nullptr), edgeIdx_(1, dev), destroyStream_(false) {
     CUDA_RUNTIME(cudaSetDevice(dev_));
     CUDA_RUNTIME(cudaMallocManaged(&count_, sizeof(*count_)));
     zero_async<1>(count_, dev_, stream_); // zero on the device that will do the counting
@@ -213,7 +233,8 @@ public:
   }
 
   EdgeWarpDynTC(EdgeWarpDynTC &&other)
-      : dev_(other.dev_), stream_(other.stream_), count_(other.count_), destroyStream_(other.destroyStream_) {
+      : dev_(other.dev_), stream_(other.stream_), count_(other.count_), edgeIdx_(std::move(other.edgeIdx_)),
+        destroyStream_(other.destroyStream_) {
     other.count_ = nullptr;
     other.destroyStream_ = false;
     other.stream_ = nullptr;
@@ -229,27 +250,29 @@ public:
   }
 
   template <typename CsrCoo>
-  void count_async(const CsrCoo &mat, const size_t edgeOffset, const size_t numEdges, const size_t dimBlock = 256) {
+  void count_async(const CsrCoo &adj, const size_t edgeOffset, const size_t numEdges, const size_t dimBlock = 256) {
     assert(count_);
-    assert(edgeOffset + numEdges <= mat.nnz());
+    assert(edgeOffset + numEdges <= adj.nnz());
+    assert(edgeIdx_.data());
     CUDA_RUNTIME(cudaSetDevice(dev_));
     CUDA_RUNTIME(cudaStreamSynchronize(stream_));
     zero_async<1>(count_, dev_, stream_);
     CUDA_RUNTIME(cudaGetLastError());
     device_fill(edgeIdx_.data(), 1, edgeOffset);
-    // zero_async<1>(count_, dev_, stream_); // zero on the device that will do the counting
-    // uint64_t *devCount = nullptr;
-    // CUDA_RUNTIME(cudaHostGetDevicePointer(&devCount, count_, 0));
-    // LOG(debug, "zero {}", uintptr_t(count_));
-    // *count_ = 0;
-    // LOG(debug, "did zero");
+    CUDA_RUNTIME(cudaGetLastError());
 
 #define CASE(const_dimBlock)                                                                                           \
   case const_dimBlock: {                                                                                               \
-    const int dimGrid = (numEdges + dimBlock - 1) / const_dimBlock;                                                    \
-    LOG(debug, "device = {}, tc_edge_dyn_kernel<<<{}, {}, 0, {}>>>", dev_, dimGrid, const_dimBlock, uintptr_t(stream_));                 \
+    int maxActiveBlocks;                                                                                               \
+    CUDA_RUNTIME(cudaOccupancyMaxActiveBlocksPerMultiprocessor(                                                        \
+        &maxActiveBlocks, tc_edge_dyn_kernel<const_dimBlock, CsrCoo>, const_dimBlock, 0));                             \
+    cudaDeviceProp props;                                                                                              \
+    CUDA_RUNTIME(cudaGetDeviceProperties(&props, dev_));                                                               \
+    const int dimGrid = maxActiveBlocks * props.multiProcessorCount;                                                   \
+    LOG(debug, "device = {}, tc_edge_dyn_kernel<<<{}, {}, 0, {}>>>", dev_, dimGrid, const_dimBlock,                    \
+        uintptr_t(stream_));                                                                                           \
     tc_edge_dyn_kernel<const_dimBlock>                                                                                 \
-        <<<dimGrid, const_dimBlock, 0, stream_>>>(count_, mat, numEdges, edgeOffset, edgeIdx_.data());                 \
+        <<<dimGrid, const_dimBlock, 0, stream_>>>(count_, adj, numEdges, edgeOffset, edgeIdx_.data());                 \
     break;                                                                                                             \
   }
 
@@ -268,8 +291,8 @@ public:
     CUDA_RUNTIME(cudaGetLastError());
   }
 
-  template <typename CsrCoo> uint64_t count_sync(const CsrCoo &mat, const size_t edgeOffset, const size_t n) {
-    count_async(mat, edgeOffset, n);
+  template <typename CsrCoo> uint64_t count_sync(const CsrCoo &adj, const size_t edgeOffset, const size_t n) {
+    count_async(adj, edgeOffset, n);
     sync();
     return count();
   }
