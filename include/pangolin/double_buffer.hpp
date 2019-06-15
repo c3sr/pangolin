3948a9eae381cc1cf394c2f19a2fc97b5c04f5c5
Carl Pearson <pearson@illinois.edu>
2019-06-15 13:01:29 -0500

    better testing and fixes for double-buffer

--- include/pangolin/double_buffer.hpp
+++ include/pangolin/double_buffer.hpp
@@ -1,8 +1,14 @@
 #pragma once
 
 #include <vector>
+#include <atomic>
 
 #include "logger.hpp"
+
+
+// FIXME:
+// could the consumer ready be signaled by an empty consumer buffer?
+
 
 namespace pangolin {
 
@@ -18,27 +24,27 @@ template <typename T, size_t N = 512> struct DoubleBuffer {
 private:
 
 
-  volatile bool producer_ready_;
-  volatile bool consumer_ready_;
+  std::atomic<bool> producer_ready_;
+  std::atomic<bool> consumer_ready_;
 
   std::mutex mtx_;
   std::condition_variable consumerBusy; //<! block producer from flipping
   std::condition_variable producerBusy; //<! block consumers until flipped
   
-  volatile bool close_; //!< true if new values will never be added to the queue
+  std::atomic<bool> close_; //!< true if new values will never be added to the queue
 
 public:
   std::vector<T> produce;
   std::vector<T> consume;
 
-  DoubleBuffer() : close_(false) {
+  DoubleBuffer() : producer_ready_(false), consumer_ready_(false), close_(false) {
     produce.reserve(N);
     consume.reserve(N);
   }
 
   void flip() {
     std::swap(produce, consume);
-    produce.clear();
+    produce.clear(); // produce buffer should now be empty
     producer_ready_ = false;
     consumer_ready_ = false;
   }
@@ -56,16 +62,23 @@ public:
     lock.unlock();
   }
 
+/*
+
+
+
+ */
+
+
   void notify_consumer() {
-    producer_ready_ = true;
-    producerBusy.notify_all();
+    producer_ready_ = true; // the producer has prepared the consumer buffer
+    producerBusy.notify_all(); // release anyone waitin on the producer
   }
 
   void wait_consumer() {
     std::unique_lock<std::mutex> lock(mtx_);
 
     // wait for the consumer to signal he's done with the buffer
-    consumerBusy.wait(lock, [this]() { return consumer_ready_; });
+    consumerBusy.wait(lock, [this]() { return consumer_ready_.load(); });
 
     // once the consumer says he's done, clear it so if we ask again we have to wait again
     consumer_ready_ = false;
@@ -73,9 +86,12 @@ public:
     lock.unlock();
   }
 
+/*! let the producer know the consumer is done
+ */
   void notify_producer() {
-    consumer_ready_ = true;
-    consumerBusy.notify_all();
+    consume.clear(); // consumer is done with this buffer
+    consumer_ready_ = true; // consumer is ready for more stuff
+    consumerBusy.notify_all(); // release anyone waiting on the consumer
   }
 
   size_t consume_size() {
@@ -91,7 +107,7 @@ public:
     producerBusy.notify_all();
   }
 
-  bool is_closed() const { return close_; }
+  bool is_closed() const { return close_.load(); }
 
 
 };
