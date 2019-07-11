bfc220d05bb7d3c185c03f3e4f38f647b38a982f
Carl Pearson <pearson@illinois.edu>
2019-07-11 06:49:17 -0500

    add warp_min and warp_max

--- include/pangolin/algorithm/reduction.cuh
+++ include/pangolin/algorithm/reduction.cuh
@@ -19,4 +19,42 @@ template <typename T> __device__ __forceinline__ T warp_sum(T val) {
   return val;
 }
 
+template <typename T> __device__ __forceinline__ T warp_max(T val) {
+
+#if __CUDACC_VER_MAJOR__ >= 9
+#pragma unroll
+  for (int offset = 16; offset > 0; offset /= 2) {
+    T other = __shfl_down_sync(0xffffffff, val, offset);
+    val = other > val ? other : val;
+  }
+#else
+#pragma unroll
+  for (int offset = 16; offset > 0; offset /= 2) {
+    T other += __shfl_down(0xffffffff, val, offset);
+    val = other > val ? other : val;
+  }
+#endif
+
+  return val;
+}
+
+template <typename T> __device__ __forceinline__ T warp_min(T val) {
+
+#if __CUDACC_VER_MAJOR__ >= 9
+#pragma unroll
+  for (int offset = 16; offset > 0; offset /= 2) {
+    T other = __shfl_down_sync(0xffffffff, val, offset);
+    val = other < val ? other : val;
+  }
+#else
+#pragma unroll
+  for (int offset = 16; offset > 0; offset /= 2) {
+    T other += __shfl_down(0xffffffff, val, offset);
+    val = other < val ? other : val;
+  }
+#endif
+
+  return val;
+}
+
 } // namespace pangolin
