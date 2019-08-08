134803e0deb690ded49653037f096b3c39cdb52b
Carl Pearson <pearson@illinois.edu>
2019-08-08 14:44:05 -0500

    add const functions

--- include/pangolin/dense/array_view.hpp
+++ include/pangolin/dense/array_view.hpp
@@ -7,20 +7,21 @@
 #endif
 
 template <typename T> class ArrayView {
-  T *data_;
+  const T *data_;
   size_t size_;
 
 public:
-  __PANGOLIN_HOST_DEVICE__ ArrayView(T *data, size_t size) : data_(data), size_(size) {}
+  __PANGOLIN_HOST_DEVICE__ ArrayView(const T *data, size_t size) : data_(data), size_(size) {}
   __PANGOLIN_HOST_DEVICE__ ArrayView() : ArrayView(nullptr, 0) {}
 
   __PANGOLIN_HOST_DEVICE__ __forceinline__ T &operator[](size_t i) noexcept { return data_[i]; }
   __PANGOLIN_HOST_DEVICE__ __forceinline__ const T &operator[](size_t i) const noexcept { return data_[i]; }
 
-  __PANGOLIN_HOST_DEVICE__ __forceinline__ T *begin() const noexcept { return data_; }
-  __PANGOLIN_HOST_DEVICE__ __forceinline__ T *end() const noexcept { return data_ + size_; }
+  __PANGOLIN_HOST_DEVICE__ __forceinline__ const T *begin() const noexcept { return data_; }
+  __PANGOLIN_HOST_DEVICE__ __forceinline__ const T *end() const noexcept { return data_ + size_; }
 
   __PANGOLIN_HOST_DEVICE__ __forceinline__ size_t size() const noexcept { return size_; }
+  __PANGOLIN_HOST_DEVICE__ __forceinline__ const T *data() const noexcept { return data_; }
 };
 
 #undef __PANGOLIN_HOST_DEVICE__
