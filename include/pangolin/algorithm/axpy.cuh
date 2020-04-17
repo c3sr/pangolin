cc4c9b3001f2806cff945fdac6194e32fe667d67
Carl Pearson <carl.w.pearson@gmail.com>
2020-04-17 12:54:54 -0500

    fix comments

--- include/pangolin/algorithm/axpy.cuh
+++ include/pangolin/algorithm/axpy.cuh
@@ -5,7 +5,7 @@
 
 namespace pangolin {
 
-/*! a[i] = a[i] * x + y
+/*! a[i] += y
  */
 template <size_t GRID_DIM_X, size_t BLOCK_DIM_X, typename T> __device__ void grid_apy(T *a, const T y, const size_t n) {
   for (size_t i = BLOCK_DIM_X * blockIdx.x + threadIdx.x; i < n; i += GRID_DIM_X * BLOCK_DIM_X) {
@@ -13,10 +13,10 @@ template <size_t GRID_DIM_X, size_t BLOCK_DIM_X, typename T> __device__ void gri
   }
 }
 
-/*! a[i] = a[i] * x + y
+/*! a[i] += y
  */
 template <size_t GRID_DIM_X, size_t BLOCK_DIM_X, typename T>
-__global__ void axpy_async_kernel(T *a, const T y, const size_t n) {
+__global__ void apy_async_kernel(T *a, const T y, const size_t n) {
   grid_apy<GRID_DIM_X, BLOCK_DIM_X>(a, y, n);
 }
 
@@ -27,7 +27,7 @@ template <typename T> void device_axpy_async(T *a, const T x, const T y, const s
     constexpr size_t dimGrid = 150;
     constexpr size_t dimBlock = 512;
     LOG(debug, "launch apy_async_kernel<<<{}, {}, 0, {}>>>", dimGrid, dimBlock, uintptr_t(stream));
-    axpy_async_kernel<dimGrid, dimBlock><<<dimGrid, dimBlock, 0, stream>>>(a, y, n);
+    apy_async_kernel<dimGrid, dimBlock><<<dimGrid, dimBlock, 0, stream>>>(a, y, n);
     CUDA_RUNTIME(cudaGetLastError());
   } else {
     LOG(error, "unimplemented!");
