2d7b30fd8ba04a96accee83dcc83a987aa8faa7b
Carl Pearson <carl.w.pearson@gmail.com>
2020-04-20 13:55:11 -0500

    travis for amd64 and ppc64le

---
+++ ci/install.sh
@@ -0,0 +1,42 @@
+set -x -e
+
+source ci/env.sh
+
+# Install Cmake if it doesn't exist
+mkdir -p $CMAKE_PREFIX
+if [[ ! -f $CMAKE_PREFIX/bin/cmake ]]; then
+    if [[ $TRAVIS_CPU_ARCH == "ppc64le" ]]; then
+        wget -qSL https://cmake.org/files/v3.13/cmake-3.13.5.tar.gz -O cmake.tar.gz
+        tar -xf cmake.tar.gz --strip-components=1 -C $CMAKE_PREFIX
+        rm cmake.tar.gz
+        cd $CMAKE_PREFIX
+        ./bootstrap --prefix=$CMAKE_PREFIX
+        make -j `nproc` install
+    elif [[ $TRAVIS_CPU_ARCH == "amd64" ]]; then
+        wget -qSL https://cmake.org/files/v3.13/cmake-3.13.5-Linux-x86_64.tar.gz -O cmake.tar.gz
+        tar -xf cmake.tar.gz --strip-components=1 -C $CMAKE_PREFIX
+        rm cmake.tar.gz
+    fi
+fi
+cd $HOME
+
+sudo apt-get update 
+sudo apt-get install -y --no-install-recommends \
+  libopenmpi-dev openmpi-bin
+
+## install CUDA
+sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
+
+if [[ $TRAVIS_CPU_ARCH == "ppc64le" && $CUDA_VERSION == "102" && TRAVIS_DIST == "bionic" ]]; then
+    CUDA_URL="https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/ppc64el/cuda-repo-ubuntu1804_10.2.89-1_ppc64el.deb"
+elif [[ $TRAVIS_CPU_ARCH == "amd64" && $CUDA_VERSION == "102" && TRAVIS_DIST == "bionic" ]]; then
+    CUDA_URL="http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-repo-ubuntu1804_10.2.89-1_amd64.deb"
+elif [[ $TRAVIS_CPU_ARCH == "amd64" && $CUDA_VERSION == "102" && TRAVIS_DIST == "trusty" ]]; then
+    CUDA_URL="http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/cuda-repo-ubuntu1604_10.2.89-1_amd64.deb"
+fi
+
+wget -SL $CUDA_URL -O cuda.deb
+sudo dpkg -i cuda.deb
+sudo apt-get update 
+sudo apt-get install -y --no-install-recommends \
+  cuda-toolkit-10-2
---
+++ ci/script.sh
@@ -0,0 +1,19 @@
+set -x -e
+
+source ci/env.sh
+
+cd ${TRAVIS_BUILD_DIR}
+
+which g++
+which nvcc
+which cmake
+
+g++ --version
+nvcc --version
+cmake --version
+
+mkdir build
+cd build
+cmake .. \
+  -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
+make VERBOSE=1 
